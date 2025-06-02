3767fc7637e1de600c6dbba714c0f3897af0a7d5
Russ DEV <niahui.dev25@gmail.com>
2025-06-02 00:28:36 -0500

    cramos el archivo video_scrollable_view y se modificó el main, discover_provider y discover_screen

--- lib/src/presentation/providers/discover_provider.dart
+++ lib/src/presentation/providers/discover_provider.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/src/domain/entities/video_post.dart';
+import 'package:toktik/src/infrastructure/models/local_video_model.dart';
+import 'package:toktik/src/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // Todo: Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    //Todo: cargar videos
+    await Future.delayed(Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
